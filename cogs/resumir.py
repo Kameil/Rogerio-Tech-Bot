5fd942aef92d333ec40d230b1024e325a76a25c3
kameil <racovikpvpbr@gmail.com>
2025-03-30 18:29:51 -0300

    mergeado com a main

--- cogs/resumir.py
+++ cogs/resumir.py
@@ -5,7 +5,13 @@ import discord
 class Resumir(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.model = bot.model
+        self.generation_config = bot.generation_config
 
+    @commands.Cog.listener(name="resumir")
+    async def rogeriootechpro(self, inter: discord.Interaction):
+        await inter.response.send_message("Rogerio Tech")
+    
 
 
 async def setup(bot: commands.Bot):
