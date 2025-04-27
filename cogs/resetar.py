73b88fdf6da5104ed014a5bb1fbcd2d597b80946
cafewhaze <contaparaaescola60@gmail.com>
2025-04-26 22:10:28 -0300

    resumir.py - organizado

--- cogs/resetar.py
+++ cogs/resetar.py
@@ -2,6 +2,7 @@ from discord import app_commands
 from discord.ext import commands
 import discord
 
+
 class Resetar(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,23 +11,32 @@ class Resetar(commands.Cog):
         self.chats = bot.chats
         self.client = bot.client
 
-    @app_commands.command(name='resetar', description='Resetar a conversa com o bot no canal atual.')
-    async def pedra(self, inter: discord.Interaction):
+    @app_commands.command(name="resetar", description="Resetar a conversa com o bot no canal atual.")
+    async def resetar(self, inter: discord.Interaction):
         try:
             await inter.response.defer()
             channel_id = str(inter.channel.id)
-            if channel_id in self.chats:
-                self.chats[channel_id] = self.client.aio.chats.create(model=self.model, config=self.generation_config)
-                embed = discord.Embed(title="Conversa resetada", description="A conversa com o bot foi resetada com sucesso.", color=discord.Color.green())
 
+            if channel_id in self.chats:
+                self.chats[channel_id] = self.client.aio.chats.create(
+                    model=self.model,
+                    config=self.generation_config
+                )
+                embed = discord.Embed(
+                    title="Conversa resetada",
+                    description="A conversa com o bot foi resetada com sucesso.",
+                    color=discord.Color.green()
+                )
                 embed.set_footer(text=f"{inter.user.name}")
                 await inter.followup.send(embed=embed)
-
-
             else:
-                await inter.followup.send("Nao ha conversa para resetar.")
+                await inter.followup.send("Não há conversa para resetar.")
         except Exception as e:
-            embed = discord.Embed(title="Ocorreu Um Erro!", description=f"\n```py\n{str(e)}\n```", color=discord.Color.red())
+            embed = discord.Embed(
+                title="Ocorreu Um Erro!",
+                description=f"\n```py\n{str(e)}\n```",
+                color=discord.Color.red()
+            )
             await inter.followup.send(embed=embed)
 
 
